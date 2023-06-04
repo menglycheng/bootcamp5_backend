3a584037aed76e6261caa8082737ff37c3b69694
SambathSoth <soth.sambath19@kit.edu.kh>
2023-06-04 14:36:16 +0700

    initial prject

---
+++ src/main/java/com/checkme/CheckMe/CheckMeApplication.java
@@ -0,0 +1,13 @@
+package com.checkme.CheckMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkme/CheckMe/CheckMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkme.CheckMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
